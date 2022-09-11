96f22888386c5616d43bab75bf9e63db45949018
Kevin Lin <klin2358@gmail.com>
2022-09-11 11:21:29 -0400

    countdown timer and css

--- react-app/src/components/Timer.js
+++ react-app/src/components/Timer.js
@@ -1,41 +1,71 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState, useRef } from 'react';
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 
-class Timer extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      count: 30
+function Timer(){
+  const intervalRef = useRef(null);
+  const [timer, setTimer] = useState('00');
+  function getTimeRemaining(endtime){
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((total/1000) % 60);
+    const minutes = Math.floor((total/1000/60) %60);
+    const hours = Math.floor((total/1000*60*60) %24);
+    const days = Math.floor(total/(1000*60*60*24));
+    return{
+      total, days, hours, minutes, seconds
+    }
+  }
+  function startTimer(deadline){
+    let {total, days, hours, minutes, seconds } = getTimeRemaining(deadline);
+    if(total>=0){
+      setTimer(
+        // (hours > 9 ? hours : '0'+hours) + ':' +
+        // (minutes > 9 ? minutes : '0'+minutes)+ ':' +
+        (seconds > 9 ? seconds : '0'+seconds)
+      )
+    }else{
+      clearInterval(intervalRef.current);
     }
   }
 
-  render () {
-    const {count} = this.state
-    return (
-      <div>
-        <h1>{count}</h1>
-      </div>
-    )
-  }
-  // setInterval
-  // clearInterval
-  componentDidMount () {
-    this.setState({
-      count: 10
-    })
-    this.doIntervalChange()
-  }
-
-  doIntervalChange = () => {
-      this.myInterval = setInterval(() => {
-      this.setState(prevState => ({
-        count: prevState.count - 1
-      }))
+  function clearTimer(endtime){
+    setTimer('30');
+    if(intervalRef.current) clearInterval(intervalRef.current);
+    const id = setInterval(()=>{
+      startTimer(endtime);
     }, 1000)
+    intervalRef.current = id;
   }
 
-  componentWillUnmount () {
-    clearInterval(this.myInterval)
+  // set timer
+  function getDeadlineTime(){
+    let deadline = new Date();
+    deadline.setSeconds(deadline.getSeconds()+30);
+    return deadline;
   }
+  
+  useEffect(() => {
+    clearTimer(getDeadlineTime());
+    return () => {if(intervalRef.current) clearInterval(intervalRef.current)}
+  }, []);
+
+  function onClickResetBtn(){
+    clearTimer(getDeadlineTime());
+  }
+
+  return(
+    <div id="countdown">
+      <CountdownCircleTimer
+        isPlaying
+        duration={7}
+        colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+        colorsTime={[7, 5, 2, 0]}
+        size={50}
+      >
+        {({ timer }) => timer}
+      </CountdownCircleTimer>
+    </div>
+
+  )
 }
 
-export default Timer
+export default Timer;
